6ace06ce644b1e045984f2d5eccdf942f762d37c
Harish Kumar <harishstudio877@gmail.com>
2025-01-03 08:43:39 +0530

    express routing

--- Express/index.js
+++ Express/index.js
@@ -4,6 +4,14 @@ const app = express();
 
 const port = 3000;
 
+app.get('/', (req, res) => {
+    res.send("welcome to express class!");
+});
+
+app.get('/about', (req, res) => {
+    res.send("This is about page");
+});
+
 app.listen(port, () => {
     console.log(`listening on port ${port}...`);
 })
