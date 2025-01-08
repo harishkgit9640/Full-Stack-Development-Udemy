4237fa1fb7912a8b1ec2638cd571c6102af19daa
Harish Kumar <harishstudio877@gmail.com>
2025-01-08 07:48:25 +0530

    user and video models are added

--- vedtube/src/app.js
+++ vedtube/src/app.js
@@ -1,4 +1,24 @@
 import express from 'express';
 const app = express();
+import cors from 'cors';
+
+app.use(
+    cors({
+        origin: process.env.CORS_ORIGIN,
+        credentials: true,
+    })
+)
+
+// common middleware
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"))
+
+
+// import routes
+import healthCheckRouter from '../routers/healthcheck.route.js'
+
+// routes
+app.use("/api/v1/healthcheck", healthCheckRouter)
 
 export default app
