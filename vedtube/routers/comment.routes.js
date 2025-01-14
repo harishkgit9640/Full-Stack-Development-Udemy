6202a419ad800655cdf788fda60547e51b81f98a
Harish Kumar <harishstudio877@gmail.com>
2025-01-14 20:45:33 +0530

    comment routes

--- vedtube/routers/comment.routes.js
+++ vedtube/routers/comment.routes.js
@@ -1,8 +1,15 @@
 import { Router } from "express";
-import { addComment } from '../controllers/comment.controllers.js';
+import { addComment, getComments, getCommentById, updateComment, deleteComment } from '../controllers/comment.controllers.js';
+import { verifyJWT } from "../middlewares/auth.middlewares.js";
 
 const router = Router()
-router.route('/add').post(addComment);
+
+router.route(verifyJWT)
+router.route('/:videoId').post(addComment);
+router.route('/get-comments').get(getComments);
+router.route('/:commentId').get(getCommentById);
+router.route('/:commentId').patch(updateComment);
+router.route('/:commentId').delete(deleteComment);
 
 
 export default router
