a254b908927b188e6c7981caba1240a6dac0feca
Harish Kumar <harishstudio877@gmail.com>
2025-01-16 22:16:28 +0530

    video routes added

--- vedtube/routers/videos.routes.js
+++ vedtube/routers/videos.routes.js
@@ -4,5 +4,6 @@ import { verifyJWT } from "../middlewares/auth.middlewares";
 const router = express.Router();
 
 router.route(verifyJWT)
-router.route('/like').post();
-router.route('/unlike').post();
+router.route('/add-video').post();
+router.route('/get-videos').get();
+router.route('/get-videos/:videoId').get();
